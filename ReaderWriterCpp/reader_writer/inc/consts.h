5e9e2db5df9f10f2dcf906582fa72e401bbf1a17
Patrick Kauls <patrick.kauls@dap.rwth-aachen.de>
2022-04-06 14:06:45 +0200

    reformat and add documentation to consts

--- ReaderWriterCpp/reader_writer/inc/consts.h
+++ ReaderWriterCpp/reader_writer/inc/consts.h
@@ -3,5 +3,8 @@
 #include <array>
 
 namespace open_vector_format::reader_writer {
-    const std::array<uint8_t, 4> kMagicBytes{ { 0x4c, 0x56, 0x46, 0x21 } };
+
+/** Magic bytes at the beginning of open vector format files. */
+const std::array<uint8_t, 4> kMagicBytes{ { 0x4c, 0x56, 0x46, 0x21 } };
+
 }
